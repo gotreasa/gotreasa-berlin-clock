de1620f5458804a2c75e634107051bf3724d8b1c
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-07-11 00:19:12 +0100

    test: validate the image updates

--- src/api/app.js
+++ src/api/app.js
@@ -10,6 +10,8 @@ app.use(helmet());
 
 app.use('/health', (_, response) => response.sendStatus(200));
 
+app.use('/test', (_, response) => response.sendStatus(200));
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiSpecification));
 
 app.get('/api/v1/time/:time', (req, response) => {
