53d14089ed39aa91c260e454c56bb67947dd9520
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-07-24 02:26:17 +0100

    chore: setup each of the prettier plugins

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,9 @@ module.exports = {
   printWidth: 80,
   singleQuote: true,
   trailingComma: 'all',
+  plugins: [
+    'prettier-plugin-gherkin',
+    '@prettier/plugin-ruby',
+    'prettier-plugin-sh',
+  ],
 };
