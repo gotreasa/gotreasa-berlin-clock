8a9f8afe3bb5ca369b575e44b15b8e7ae076acc7
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-08-09 18:27:33 +0100

    refactor: include the timestamp in the app startup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const app = require('./src/api/app');
+const { timestamp } = require('./timestamp.json');
 
 const port = process.env.SERVER_PORT;
 
@@ -6,7 +7,7 @@ const server = app.listen(port, () => {
   console.log(
     `🚀 Template NodeJS app listening at http://localhost:${
       server.address().port
-    }`,
+    } built at '${timestamp}'`,
   );
 });
 
