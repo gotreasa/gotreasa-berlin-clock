0e1b70dfe75881dde0c2ca9fc2d053fac53dd6b7
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-08-11 02:47:10 +0100

    feat: enable csrf and cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,15 +1,24 @@
 const { exec } = require('child_process');
+const crypto = require('crypto');
 const express = require('express');
 const helmet = require('helmet');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const swaggerUi = require('swagger-ui-express');
 const openApiSpecification = require('../../openapi.json');
+
+const COOKIES_SECRET = crypto.randomBytes(32).toString('hex');
 const getTime = require('./Time');
 
+const csrfProtect = csrf({ cookie: true });
 const app = express();
 app.use(helmet());
+app.use(cors());
+app.use(cookieParser(COOKIES_SECRET));
 
-app.use('/health', (_, response) => {
+app.use('/health', csrfProtect, (_, response) => {
   exec('/usr/bin/test -f "/goss/goss" && /goss/goss validate', (error) => {
     console.log('Health check output', error);
 
@@ -17,9 +26,14 @@ app.use('/health', (_, response) => {
   });
 });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiSpecification));
+app.use(
+  '/api-docs',
+  csrfProtect,
+  swaggerUi.serve,
+  swaggerUi.setup(openApiSpecification),
+);
 
-app.get('/api/v1/time/:time', (req, response) => {
+app.get('/api/v1/time/:time', csrfProtect, (req, response) => {
   try {
     return response.status(200).json(getTime(req.params.time));
   } catch (error) {
