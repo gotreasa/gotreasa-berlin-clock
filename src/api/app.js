ddeda2ab293b5cd0a0a80fc3096e80d00ba2e26e
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-08-11 19:11:51 +0100

    chore: investiage fix for the proxy

--- src/api/app.js
+++ src/api/app.js
@@ -26,6 +26,8 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(cookieParser(COOKIES_SECRET));
+app.set('trust proxy', 1);
+app.get('/ip', (request, response) => response.send(request.ip));
 
 app.use('/health', limiter, csrfProtect, (_, response) => {
   exec('/usr/bin/test -f "/goss/goss" && /goss/goss validate', (error) => {
